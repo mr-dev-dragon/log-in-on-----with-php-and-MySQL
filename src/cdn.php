c00a64d71932dc596545d48d17440b1115632958
Ahmed Aoulad Aamar <aouladoumarahmed100@gmail.com>
2022-02-25 23:10:46 +0100

    add form with bootstrap

---
+++ src/cdn.php
@@ -0,0 +1,10 @@
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- Font Awesome -->
+<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" rel="stylesheet" />
+<!-- Google Fonts -->
+<link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" rel="stylesheet" />
+<!-- MDB -->
+<link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/3.10.2/mdb.min.css" rel="stylesheet" />
+  <title>log-in</title>
